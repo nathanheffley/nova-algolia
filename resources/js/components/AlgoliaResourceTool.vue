0300f8c5fb3ad1b841a3ba1df1dfa5f52f1a5e47
Nathan Heffley <nathanheffley@gmail.com>
2018-08-29 12:18:54 -0400

    Show loader in resource tool

--- resources/js/components/AlgoliaResourceTool.vue
+++ resources/js/components/AlgoliaResourceTool.vue
@@ -1,8 +1,10 @@
 <template>
-    <div>
+    <loading-card v-if="loading" class="h-12 shadow-none"></loading-card>
+    <div v-else>
         <h3 class="mr-3 text-base text-80 font-bold">Index</h3>
         <div class="flex items-center mb-4">
             <p class="text-4xl mr-auto" v-text="algoliaData.index"></p>
+
             <button @click="importRecord" title="Import" class="appearance-none cursor-pointer text-70 hover:text-primary mr-3"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" aria-labelledby="import" role="presentation" class="fill-current"><path fill-rule="nonzero" d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z"></path></svg></button>
             <button @click="openConfirmRemoval" title="Remove" class="appearance-none cursor-pointer text-70 hover:text-danger mr-3"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" aria-labelledby="delete" role="presentation" class="fill-current"><path fill-rule="nonzero" d="M6 4V2a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2h5a1 1 0 0 1 0 2h-1v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6H1a1 1 0 1 1 0-2h5zM4 6v12h12V6H4zm8-2V2H8v2h4zM8 8a1 1 0 0 1 1 1v6a1 1 0 0 1-2 0V9a1 1 0 0 1 1-1zm4 0a1 1 0 0 1 1 1v6a1 1 0 0 1-2 0V9a1 1 0 0 1 1-1z"></path></svg></button>
         </div>
@@ -35,14 +37,16 @@
         data: function () {
             return {
                 algoliaData: [],
-                path: `/nova-vendor/nova-algolia/${this.resourceName}/${this.resourceId}`,
+                loading: true,
                 showingConfirmRemoval: false,
+                path: `/nova-vendor/nova-algolia/${this.resourceName}/${this.resourceId}`,
             }
         },
 
         mounted() {
             Nova.request(this.path).then(({data}) => {
                 this.algoliaData = data;
+                this.loading = false;
             });
         },
 
